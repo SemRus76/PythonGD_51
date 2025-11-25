8cfb763c1ceed420aca0d5a7837185fe85ba3aba
Семен Журавлев <zsocom@mail.ru>
2025-11-25 21:35:34 +0300

    [ADD]:[GIT] Создание репозитория Git - Git Init

---
+++ calc.py
@@ -0,0 +1,20 @@
+def sum(first: int, second: int) -> int:
+    """
+        Функция для суммирования чисел
+
+        :param first : Первое число
+        :param second : Второе число
+        :return : Сумма чисел
+    """
+    return first + second
+
+def sub (first : int, second : int) -> int:
+    return first - second
+
+def mult (first : int, second : int) -> int:
+    return first * second
+
+def div (first : int , second : int) -> int:
+    return first / second
+
+
