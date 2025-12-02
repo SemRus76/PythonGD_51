443d9de417b3219d5e90568aa754058907c7da24
Семен Журавлев <zsocom@mail.ru>
2025-12-02 20:50:49 +0300

    [REF]:[MAIN] Продлжаем разводить ветки

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 
 print("Hello Github")
-print("Hello Github")
+
 print("Hello Github")
 
 print("Hello World")
 print("Hello World")
-print("Hello World")
 
 print("Hello World")
 print("Hello GD_51")
 print("Hello GD_51")
-print("Hello GD_51")
-print("Hello GD_51")
+
 
 
 
