d75cd7f949d708be66804e15b5a9b719e8a990ff
Семен Журавлев <zsocom@mail.ru>
2025-12-16 21:24:46 +0300

    [ADD]:[Наследование] Добавлено описание Наследования в Python

--- file.py
+++ file.py
@@ -1,1 +1,26 @@
-print("Hello Github")
+# Наследование
+#
+# Третий принцип ООП - Наследование - позволяет создавать классы на
+#   базе уже имеющихся
+from temp import Temp
+
+class Template(Temp):
+
+    __name = str()
+
+    def __init__(self, name: str):
+        super().__init__(10, 2)
+        self.__name = name
+
+    def __del__(self):
+        pass
+
+    def __str__(self):
+        return self.__name + f" {super().__str__()}"
+
+
+
+
+
+
+
