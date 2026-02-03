d2c1067b67d8be4f534ce3251b7892ea1bf7e91a
Семен Журавлев <zsocom@mail.ru>
2026-02-03 21:20:07 +0300

    [REF]:[DataBase] Добавлен пример использования psycopg2 и подключения к PostgresDB

---
+++ JSON.py
@@ -0,0 +1,23 @@
+import json
+
+json_obj = dict()
+
+with open("template.json", "r") as json_file:
+    json_obj = json.load(json_file)
+    print(json_obj["value-float"])
+    print(json_obj["value-array"][3]["city"])
+    print(json_obj["123"])
+    print(json_obj)
+
+json_obj["age"] = 18
+json_obj[123] = "hello"
+
+with open("template.json", "w+") as json_file:
+    json.dump(json_obj, json_file)
+
+print("-===================-")
+str_json = json.dumps(json_obj)
+print(json_obj)
+print(str_json)
+json_obj = json.loads(str_json)
+print("-===================-")
--- main.py
+++ main.py
@@ -1,29 +1,70 @@
-import json
+import psycopg2
 
-json_obj = dict()
+connection_item = psycopg2.connect(
+        dbname="postgres",
+        user="postgres",
+        password="postgres",
+        host="localhost",
+        port=5432
+    )
+username = "Иванов"
+query = ("SELECT                             "
+         "  first_name as Имя                "
+         " ,second_name as Фамилия           "
+         " ,last_name as Отчество            "
+         " ,age as Возраст                   "
+         " ,number as Номер_Комнаты          "
+         "FROM                               "
+         " student                           "
+         "FULL OUTER JOIN                    "
+         " hostel_room                       "
+         "ON                                 "
+         " student.id = hostel_room.student  "
+         # "WHERE                              "
+         # f" student.first_name = '{username}'"
+         )
 
-with open("template.json", "r") as json_file:
-    json_obj = json.load(json_file)
-    print(json_obj["value-float"])
-    print(json_obj["value-array"][3]["city"])
-    print(json_obj["123"])
-    print(json_obj)
+cursor = connection_item.cursor()
+cursor.execute(query)
+data = ([("Имя", "Фамилия", "Отчество", "Возраст", "Номер Комнаты")] +
+            cursor.fetchall())
+cursor.close()
+for line in data:
+    line_str = str()
+    for element in line:
+        line_str += f"{element} "
+    print(line_str)
 
-json_obj["age"] = 18
-json_obj[123] = "hello"
+student = ("( 'Лобанов', 'Семен', 'Евгеньевич', 21, true, 3 ),"
+           "( 'Семенов', 'Семен', 'Семенович', 22, true, 6 );")
+query_insert = ("INSERT INTO        "
+                " student           "
+                " (   first_name    "
+                "     ,second_name  "
+                "     ,last_name    "
+                "     ,age          "
+                "     ,sex          "
+                "     ,course       "
+                " )                 "
+	            f"VALUES {student}  "
+                )
+cursor = connection_item.cursor()
+cursor.execute(query_insert)
+connection_item.commit()
+cursor.close()
 
-with open("template.json", "w+") as json_file:
-    json.dump(json_obj, json_file)
+print("-=============================-")
 
-print("-===================-")
-str_json = json.dumps(json_obj)
-print(json_obj)
-print(str_json)
-json_obj = json.loads(str_json)
-print("-===================-")
-
-
-
-
+cursor = connection_item.cursor()
+cursor.execute(query)
+data = ([("Имя", "Фамилия", "Отчество", "Возраст", "Номер Комнаты")] +
+            cursor.fetchall())
+cursor.close()
+for line in data:
+    line_str = str()
+    for element in line:
+        line_str += f"{element} "
+    print(line_str)
+connection_item.close()
 
 
